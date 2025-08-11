a997c53df923092de70b7428a2127734b88a3c5a
youssef-ahmed25 <youssefahmed56432@gmail.com>
2025-08-11 06:47:10 +0300

    Add project files.

---
+++ Adv_c#_assig4/Program.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+
+namespace Adv_c__assig4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //int[] arr = { 1, 2, 2, 3, 1, 4, 2 };
+            //Hashtable frq = new Hashtable();
+
+            //foreach (int num in arr)
+            //{
+            //    if (frq[num] != null)
+            //    {
+            //        frq[num] = (int)frq[num] + 1;
+            //    }
+            //    else
+            //    {
+            //        frq[num] = 1;
+            //    }
+            //}
+
+            //foreach (DictionaryEntry entry in frq)
+            //{
+            //    Console.WriteLine($"Number: {entry.Key}, Frequency: {entry.Value}");
+            //} 
+            #endregion
+            #region Q2
+            //Hashtable hash = new Hashtable
+            //{
+            //    ["youssef"] = 10,
+            //    ["ahmed"] = 30,
+            //    ["ali"] = 20,
+            //    ["mohamed"] = 50
+            //};
+            //int max = 0;
+            //string maxKey = string.Empty;
+            //foreach (DictionaryEntry entry in hash)
+            //{
+            //    if ((int)entry.Value > max)
+            //    {
+            //        max = (int)entry.Value;
+            //        maxKey = (string)entry.Key;
+            //    }
+            //}
+
+            //Console.WriteLine($"max key is: {maxKey},{max}");
+            #endregion
+            #region Q3
+
+            #endregion
+            #region Q4
+
+            #endregion
+            #region Q5
+            //int[] nums = {1,2,3,4,5,5,6,6,1,1,2};
+            //int[] nums = { 1, 2, 3, 4, 5, 6};
+            //HashSet<int> table = new HashSet<int>();
+
+            //bool dup = false;
+            //foreach (int num in nums)
+            //{
+            //    if (table.Contains(num))
+            //    {
+            //        dup = true;
+            //        break;
+            //    }
+            //    table.Add(num);
+            //}
+
+            //Console.WriteLine(dup ? "duplecate found" : "No duplecate");
+            #endregion
+            #region Q6
+            //SortedDictionary<int, string> student = new SortedDictionary<int, string>
+            //{
+            //    [10] = "youssef",
+            //    [20] = "ali",
+            //    [40] = "ahmed",
+            //    [50] = "omer"
+            //};
+            //student.Remove(50);
+            //foreach (var stu in student)
+            //{
+            //    Console.WriteLine($"id: {stu.Key}, name:{stu.Value}");
+            //}
+
+            //Console.WriteLine("Student id 20: " + student[20]);
+
+            //foreach (var stu in student)
+            //{
+            //    Console.WriteLine($"id: {stu.Key}, name:{stu.Value}");
+            //}
+            #endregion
+            #region Q7
+            //SortedList <int, string>emp = new()
+            //{
+            //    [40] = "youssef",
+            //    [10] = "Mona",
+            //    [20] = "Ahmed",
+            //    [50] = "Hossam",
+            //    [30] = "Sara"
+
+            //};
+            //foreach (var e in emp)
+            //{
+            //    Console.WriteLine($"ID: {e.Key}, Name: {e.Value}");
+            //}
+            #endregion
+            #region Q8
+            //int N = 10;
+            //HashSet<int> chek = new HashSet<int>() { 1, 5, 3, 7, 4, 8 };
+            //for (int i = 1; i <= N; i++)
+            //{
+            //    if (!chek.Contains(i))
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+
+            #region Q9
+            //List<int> list = new List<int> {1,1,2,2,3,4,5,6,6};
+
+            //HashSet<int> uniqe = new HashSet<int>(list);
+            //foreach (int i in uniqe)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+            #region Q10
+
+            #endregion
+            #region Q11
+
+            #endregion
+            #region Q12
+
+            #endregion
+            #region Q13
+
+            #endregion
+            #region Q14
+
+            #endregion
+        }
+    }
+}
